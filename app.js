28f3c8bcba6eccc913f71fc881e250c728f78d1f
ashik <s2010376107@ru.ac.bd>
2025-01-13 16:53:36 +0600

    for render to deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+const compression = require('compression');
 
 // Initialize App
 const app = express();
@@ -11,20 +12,33 @@ const app = express();
 require('./config/passport')(passport);
 
 
+// Mongodb on cloud
+db_password = '6Lakak5cTioy3sSW';
+mongoose.connect('mongodb+srv://ashikurrahmanbhuiyan:6Lakak5cTioy3sSW@cluster0.89kxn.mongodb.net/smart_classroom?retryWrites=true&w=majority&appName=smart_classroom', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(err));
+
+
 // MongoDB Connection
-mongoose
-    .connect('mongodb://localhost:27017/login-system', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err));
+// mongoose
+//     .connect('mongodb://localhost:27017/login-system', {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch((err) => console.log(err));
 
 // EJS
 app.set('view engine', 'ejs');
 
 //use pablic folder for css and js
 app.use( express.static('public') );
+
+// Compression in request and response
+app.use(compression());
 
 
 // Body Parser
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+mongoose
+//     .connect('mongodb://localhost:27017/login-system', {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch((err) => console.log(err));
