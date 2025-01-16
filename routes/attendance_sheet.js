f1fbd281ecd5dcb70267ebaf7eea3a70da26ded9
ashik <s2010376107@ru.ac.bd>
2025-01-17 00:58:38 +0600

    complete the data fetch and data post to the server, now it's time to update the database

--- routes/attendance_sheet.js
+++ routes/attendance_sheet.js
@@ -4,24 +4,27 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-//connect to mongodb
-const db = mongoose.connection;
-
-
-
-router.get('/all', async(req, res) => {
+router.get('/', async(req, res) => {
     try {
         // this is not well done, this need to be change later
         const users = await students.find({ });
         res.render('attendance_sheet', { users: users });
     } catch (error) {
         console.error("Error fetching data:", error);
-        res.render('attendance_sheet');
+        res.render('attendance_sheet',{users:[]});
     }
 });
 
-router.post('/all',async (req,res)=>{
-    
+router.post('/',async (req,res)=>{
+    const attendanceArray = req.body.attendanceArray;
+    const date = req.body.date;
+    console.log(attendanceArray);
+    console.log(date);
+    res.redirect('/attendance_sheet/');
+});
+
+router.get('/all', async(req, res) => {
+    res.render('attendance_sheet_all');
 });
 
 module.exports = router;
