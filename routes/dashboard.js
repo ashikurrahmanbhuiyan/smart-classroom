8067fc23e7c7e2475079620e414a9031c7c9f269
ashik <s2010376107@ru.ac.bd>
2025-02-06 10:53:10 +0600

    adding blur, awosome

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -39,7 +39,7 @@ router.get('/teacher/dashboard', checkAuthenticatedteacher, async (req, res) => 
         todayClass = findCourses.flatMap(course =>
             course.sessionYear.flatMap(department =>
                 department.courses
-                    .filter(c => c.teacher_email === req.user.email && c.course_schedule.some(s => s.day === "Wednesday"))
+                    .filter(c => c.teacher_email === req.user.email && c.course_schedule.some(s => s.day === today))
                     .map(c => ({
                         department: course.department,
                         year_semester: department.year_semester,
