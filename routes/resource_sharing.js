ee5d5651b9d1ad5404e545e440463f99dc7c6f36
ashikurrahmanbhuiyan <s2010376107@ru.ac.bd>
2025-07-06 01:41:45 +0600

    this is the creation of the pages of the courses. add four navbar and now fixing them

--- routes/resource_sharing.js
+++ routes/resource_sharing.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log(req.user);
-    res.render('resource_sharing');
+    // res.render('course_pages/resource_sharing');
+    res.render('course_pages/layout', { page: 'resource_sharing' });
 });
 
 module.exports = router;
